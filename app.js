eff29e7010455241a80d1d93219647311a755ae7
burhan ahmed <burhanahmeed@gmail.com>
2019-12-14 14:00:55 +0700

    add react js

--- app.js
+++ app.js
@@ -20,9 +20,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', require('./routes'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 // app.use('/users', usersRouter);
  
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const Tweet = require('./tweet')
 const Public = require('./public')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.use('/api/v1', Tweet)
 
